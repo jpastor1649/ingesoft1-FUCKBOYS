836dbd6f4ae218db696586fcbeabb797ab11ab70
jpastor1649 <jpastor@unal.edu.co>
2025-07-11 20:37:35 -0500

    updated

--- Proyecto/src/models/managers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .gestor_apartamentos import GestorApartamentos
-from .gestor_pagos import GestorPagos
-from .generador_reportes import GeneradorReportes
-
-__all__ = [
-    'GestorApartamentos',
-    'GestorPagos',
-    'GeneradorReportes'
-]
