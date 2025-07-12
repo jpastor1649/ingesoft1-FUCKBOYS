2d47eaf933809f15333f344469182d0fb329955f
jpastor1649 <jpastor@unal.edu.co>
2025-07-11 21:46:37 -0500

    deleted old classes

--- Proyecto/src/models/Usuario.py
+++
@@ -1,7 +0,0 @@
-class Usuario:
-    def __init__(self, id_usuario: int, rol: str):
-        self.id_usuario = id_usuario
-        self.rol = rol  # "admin" o "inquilino"
-
-    def es_admin(self):
-        return self.rol == "admin"
